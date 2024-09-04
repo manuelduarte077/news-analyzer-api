335d916e461217f7cfe677ac0110ece8808ac96b
Manuel Duarte <carlosd99m@gmail.com>
2024-09-04 00:26:30 -0600

    feat: add firebase services

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,42 @@
 import 'package:app/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(
-    ProviderScope(child: DaktarLamara()),
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  /// Firebase Crashlytics
+  const fatalError = true;
+
+  // Non-async exceptions
+  FlutterError.onError = (errorDetails) {
+    if (fatalError) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+      // ignore: dead_code
+    } else {
+      FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    }
+  };
+
+  // Async exceptions
+  PlatformDispatcher.instance.onError = (error, stack) {
+    if (fatalError) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      // ignore: dead_code
+    } else {
+      FirebaseCrashlytics.instance.recordError(error, stack);
+    }
+    return true;
+  };
+
+  runApp(ProviderScope(child: DaktarLamara()));
 }
